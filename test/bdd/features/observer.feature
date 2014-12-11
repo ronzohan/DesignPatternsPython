c285512132f9a6fcd13d932ed2ab770eac73d0e7
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-11 16:33:02 +0800

    Finished observer acceptance test

--- test/bdd/features/observer.feature
+++ test/bdd/features/observer.feature
@@ -3,6 +3,10 @@ Feature: A simple subject that will be able to add, remove, and notify the
 		 
 		 @add_observer
 		 Scenario: Add observer
-		 Given a subject
-		 And a USA time observer "usa_time_observer" registers to subject
-		 Then subject will have "1" observer
+		 Given a subject, observer "usa_time_observer" and "eu_time_observer"
+		 And observer "usa_time_observer" and "eu_time_observer" registers to subject
+		 Then the subject notifies its observer
+		 |message                                                |
+		 |Observer usa_time_observer says: 2014-12-11 04:02:20PM |
+         |Observer eu_time_observer says: 2014-12-11 04:02:22PM  |
+		 
--- test/bdd/steps/observer_steps.py
+++ test/bdd/steps/observer_steps.py
@@ -1,20 +1,42 @@
 from lettuce import step, world
 from Observer.subject import Subject
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_in
 from Observer.observer import USATimeObserver
+from contextlib import contextmanager
+from StringIO import StringIO
+import sys
 
 
-@step(u'Given a subject')
-def given_a_observer_group1(step):
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+@step(u'Given a subject, observer "([^"]*)" and "([^"]*)"')
+def given_a_subject_observer_group1_and_group2(step, obs1, obs2):
     world.subject = Subject()
+    world.obs1 = USATimeObserver(obs1)
+    world.obs2 = USATimeObserver(obs2)
 
 
-@step(u'And a USA time observer "([^"]*)" registers to subject')
-def and_observer_group1_registers_to_subject(step, name):
-    world.observer = USATimeObserver(name)
-    world.subject.register_observer(world.observer)
+@step(u'And observer "([^"]*)" and "([^"]*)" registers to subject')
+def and_observer_group1_and_group2_registers_to_subject(step, group1, group2):
+    world.subject.register_observer(world.obs1)
+    world.subject.register_observer(world.obs2)
 
 
-@step(u'Then subject will have "([^"]*)" observer')
-def then_subject_will_have_group1_observer(step, count):
-    assert_equal(len(world.subject.observers), int(count))
+@step(u'Then the subject notifies its observer')
+def then_the_subject_notifies_its_observer(step):
+    message = []
+    for row in step.hashes:
+        message.append(str(row['message'] + '\n'))
+
+    with open('test.txt') as file:
+        for line in file:
+            assert_in(line, message)
